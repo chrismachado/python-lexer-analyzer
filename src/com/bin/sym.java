5bca893ad0bbfc4638772c07eba7a0bbeaa5181c
CHRISTIANO <christianomachado@gmail.com>
2020-01-22 21:30:43 -0300

    Symbol 'ENUMs'

---
+++ src/com/bin/sym.java
@@ -0,0 +1,59 @@
+package com.bin;
+
+public class sym {
+    public static final int L_BRACKET_OP = 42;
+    public static final int CHAR_LIT = 45;
+    public static final int EXCEPT = 19;
+    public static final int TAB_OP = 47;
+    public static final int TYPE_BOOLEAN = 35;
+    public static final int COMP_OP = 36;
+    public static final int INPUT = 24;
+    public static final int CONTINUE = 15;
+    public static final int R_PARENTHESIS_OP = 10;
+    public static final int R_BRACKET_OP = 43;
+    public static final int FOR = 21;
+    public static final int IS_NOT = 25;
+    public static final int PROPERTY_OP = 39;
+    public static final int AND = 12;
+    public static final int RANGE = 49;
+    public static final int TYPE_LIST = 30;
+    public static final int IN = 23;
+    public static final int OR = 26;
+    public static final int COMMA = 48;
+    public static final int SEMICOLON_OP = 6;
+    public static final int CLASS = 14;
+    public static final int ERROR_CHA = 5;
+    public static final int TYPE_INT = 31;
+    public static final int ERROR_ID = 3;
+    public static final int IF = 22;
+    public static final int COLON_OP = 40;
+    public static final int L_PARENTHESIS_OP = 9;
+    public static final int NUM_LIT = 11;
+    public static final int EOF = 0;
+    public static final int RETURN = 50;
+    public static final int ERROR_INV = 4;
+    public static final int error = 1;
+    public static final int LOGICAL_OP = 38;
+    public static final int TRY = 28;
+    public static final int BREAK = 13;
+    public static final int TYPE_FLOAT = 32;
+    public static final int FINALLY = 20;
+    public static final int TYPE_CHAR = 33;
+    public static final int ASSIG_OP = 37;
+    public static final int BOOLEAN_LIT = 44;
+    public static final int ELSE = 18;
+    public static final int TYPE_STRING = 34;
+    public static final int WHILE = 29;
+    public static final int ERROR_LIT = 2;
+    public static final int DEF = 16;
+    public static final int ARITH_OP = 8;
+    public static final int BIT_OP = 7;
+    public static final int STRING_LIT = 46;
+    public static final int ELIF = 17;
+    public static final int PRINT = 27;
+    public static final int IDENTIFIER = 41;
+
+    public sym() {
+    }
+}
+
