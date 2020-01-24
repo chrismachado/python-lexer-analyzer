a97db33dbbd6605399576a9ba59a06b8d0373214
CHRISTIANO <christianomachado@gmail.com>
2020-01-24 00:35:14 -0300

    Some Enums removeds

--- src/com/bin/PyTokens.java
+++ src/com/bin/PyTokens.java
@@ -1,4 +1,16 @@
 package com.bin;
 
 public enum PyTokens {
+    OPERATOR,
+    IDENTIFIER,
+    ERROR_IDENTIFIER,
+    KEYWORD,
+    LITERAL,
+    ERROR_LITERAL,
+    INVALID,
+    INVALID_CHARACTER,
+    INDENT,
+    BAD_ERROR
+
 }
+
