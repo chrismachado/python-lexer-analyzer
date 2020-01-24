a97d550e72208c9694d308f86425081ac4572531
CHRISTIANO <christianomachado@gmail.com>
2020-01-24 01:02:08 -0300

    new python code

--- out/production/PLA/files/test.py
+++ out/production/PLA/files/test.py
@@ -1,1 +1,20 @@
-a = 2
+import socket
+
+HOST = '127.0.0.1'
+PORT = 9000
+address = (HOST, PORT)
+serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+serv_socket.bind(address)
+
+serv_socket.listen(10)
+print("Aguardando conexão")
+con, client = serv_socket.accept()
+print("Conectado")
+print("Aguardando mensagem")
+data = con.recv(1024)
+print(type(data))
+print(type(data.decode()))
+serv_socket.close()
+
+
